4b5e98a9f106792262238b6bc733fc27782f62ca
Siddarth Vijay <vsiddarthvijay@gmail.com>
2022-08-11 13:49:36 +0530

    na for undefined change %

--- src/components/TradingviewChart/index.js
+++ src/components/TradingviewChart/index.js
@@ -161,7 +161,7 @@ const TradingViewChart = ({
 
       // format numbers
       let percentChange = baseChange?.toFixed(2)
-      let formattedPercentChange = (percentChange > 0 ? '+' : '') + percentChange + '%'
+      let formattedPercentChange = (percentChange > 0 ? '+' : '') + (percentChange ? (percentChange + '%') : "N/A")
       let color = percentChange >= 0 ? 'green' : 'red'
 
       // get the title of the chart
