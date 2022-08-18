b57ce6aab2e1aa1aad82e2696e206a6317c2c80d
Siddarth Vijay <vsiddarthvijay@gmail.com>
2022-08-19 04:41:38 +0530

    added side bar to lm

--- src/pages/AllPairsPage.js
+++ src/pages/AllPairsPage.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { formatTime, formattedNum, urls } from '../utils'
 import 'feather-icons'
+import { useHistory } from 'react-router-dom'
 
 import { TYPE } from '../Theme'
 import Panel from '../components/Panel'
 import { useAllPairData } from '../contexts/PairData'
-import PairList from '../components/PairList'
 import { PageWrapper, FullWrapper } from '../components'
 import { RowBetween, AutoRow, RowFixed } from '../components/Row'
 import Search from '../components/Search'
@@ -17,8 +17,16 @@ import { ThemedBackground } from '../Theme'
 import bgNoise from '../assets/bg-noise.gif'
 
 import { useMedia } from 'react-use'
-import QuestionHelper from '../components/QuestionHelper'
-import CheckBox from '../components/Checkbox'
+import Drawer from '@material-ui/core/Drawer';
+import MenuIcon from '@material-ui/icons/Menu';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { Icon, IconButton } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import { useSessionStart } from '../contexts/Application'
+import { Box } from 'rebass'
 
 const Header = styled.div`
   width: 100%;
@@ -29,6 +37,18 @@ const Header = styled.div`
 const Medium = styled.span`
   font-weight: 500;
 `
+
+const useStyles = makeStyles({
+  list: {
+    width: 250
+  },
+  fullList: {
+    width: "auto"
+  },
+  paper: {
+    background: "black",
+  }
+});
 
 const StaticOverlay = styled.div`
   -webkit-font-smoothing: antialiased;
@@ -55,6 +75,30 @@ const StaticOverlay = styled.div`
   z-index: 600;
 `
 
+const Polling = styled.div`
+  position: fixed;
+  display: flex;
+  left: 0;
+  bottom: 0;
+  padding: 1rem;
+  color: white;
+  opacity: 0.4;
+  transition: opacity 0.25s ease;
+  :hover {
+    opacity: 1;
+  }
+`
+const PollingDot = styled.div`
+  width: 8px;
+  height: 8px;
+  min-height: 8px;
+  min-width: 8px;
+  margin-right: 0.5rem;
+  margin-top: 3px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.green1};
+`
+
 function AllPairsPage() {
   const allPairs = useAllPairData()
   const markets = useGlobalMarkets()
@@ -78,13 +122,82 @@ function AllPairsPage() {
   const below400 = useMedia('(max-width: 400px)')
   const below816 = useMedia('(max-width: 816px)')
 
-  const [useTracked, setUseTracked] = useState(true)
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const seconds = useSessionStart()
+  const history = useHistory()
+
+  const sideBarText = {
+    color: "red",
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        <ListItem key={"stableswap"} disablePadding>
+          <ListItem button key={"stableswap"} onClick={() => history.push('/stableswap')}>
+            <ListItemText primaryTypographyProps={{ style: sideBarText }} primary={"> Stable Swap"} />
+          </ListItem>
+        </ListItem>
+        <ListItem key={"lendingmarket"} disablePadding>
+          <ListItem button key={"lendingmarket"} onClick={() => history.push('/lendingmarket')}>
+            <ListItemText primaryTypographyProps={{ style: sideBarText }} primary={"> Lending Market"} color="green" />
+          </ListItem>
+        </ListItem>
+      </List>
+      <Divider />
+      <Polling style={{ marginLeft: '.5rem' }}>
+        <PollingDot />
+        <a href="/" style={{ color: 'white' }}>
+          <TYPE.small color={'white'}>
+            Updated {!!seconds ? seconds + 's' : '-'} ago <br />
+          </TYPE.small>
+        </a>
+      </Polling>
+    </Box>
+  );
+
+  const classes = useStyles();
 
   return (
     <PageWrapper>
       <StaticOverlay />
       <ThemedBackground />
       <FullWrapper>
+        <div>
+          {['left'].map((anchor) => (
+            <React.Fragment key={anchor}>
+              <IconButton onClick={toggleDrawer(anchor, true)}>
+                <MenuIcon style={{ color: 'red' }} />
+              </IconButton>
+              <img src="../assets/logo.svg" />
+              <Drawer
+                classes={{ paper: classes.paper }}
+                anchor={anchor}
+                open={state[anchor]}
+                onClose={toggleDrawer(anchor, false)}
+              >
+                <img src="../assets/logo.svg" />
+                {list(anchor)}
+              </Drawer>
+            </React.Fragment>
+          ))}
+        </div>
         <AutoColumn gap="14px" style={{ paddingBottom: below800 ? '0' : '24px' }}>
           <TYPE.largeHeader>
             {below800 ? 'Lending Market Analytics' : 'Canto Lending Market Analytics'}
