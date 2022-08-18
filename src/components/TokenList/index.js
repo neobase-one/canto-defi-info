7b91a51f0b6d60dde30bb8aa04cdfbbd7b185114
Tushar Jain <>
2022-08-19 02:00:06 +0530

    changes up last graph

--- src/components/TokenList/index.js
+++ src/components/TokenList/index.js
@@ -18,6 +18,69 @@ import { TYPE } from '../../Theme'
 
 dayjs.extend(utc)
 
+const Table = styled.table`
+  border: none;
+  border: #06fc99 solid 1px;
+  box-shadow: 0px 0px 10px #94ffb241;
+  margin: 5px auto;
+  color: #06fc99;
+  width: calc(100% - 10px);
+  text-align: center;
+  border-collapse: collapse;
+  border-spacing: 0;
+  text-shadow: none;
+  thead {
+    text-transform: lowercase;
+    font-size: 14px;
+    background-color: #06fc9a1b;
+  }
+
+  th {
+    padding: 8px;
+    font-weight: 400;
+    line-height: 1rem;
+  }
+
+  tr {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 4rem;
+    background-color: black;
+    border-bottom: #06fc99 solid 1px;
+  }
+  td:first-child,
+  th:first-child {
+    padding-left: 2rem;
+    text-align: left;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    text-transform: uppercase;
+  }
+  th:first-child {
+    text-transform: lowercase;
+  }
+  img {
+    height: 30px;
+    /* width: 30px; */
+    /* border-radius: 50%; */
+    /* border: 1px solid #06fc99; */
+    /* background-color: #cecece; */
+  }
+  tbody {
+    border: #06fc99 solid 1px;
+
+    tr:hover {
+      background-color: #14392a;
+      cursor: pointer;
+    }
+  }
+  @media (max-width: 1000px) {
+    width: 800px;
+    margin: 0 2rem;
+  }
+`
+
 const PageButtons = styled.div`
   width: 100%;
   display: flex;
@@ -178,134 +241,125 @@ function TopTokenList({ tokens, itemMax = 10, useTracked = false }) {
 
   const ListItem = ({ item, index }) => {
     return (
-      <DashGrid style={{ height: '48px' }} focus={true}>
-        <DataText area="name" fontWeight="500">
-          <Row>
-            {!below680 && <div style={{ marginRight: '1rem', width: '10px' }}>{index}</div>}
-            {/* <TokenLogo address={item.id} /> */}
-            {/* <CustomLink style={{ marginLeft: '16px', whiteSpace: 'nowrap' }} to={'/token/' + item.id}> */}
-            <CustomLink style={{ marginLeft: '16px', whiteSpace: 'nowrap' }}>
-              <FormattedName
-                text={below680 ? item.symbol : item.name}
-                maxCharacters={below600 ? 8 : 16}
-                adjustSize={true}
-                link={true}
-              />
-            </CustomLink>
-          </Row>
-        </DataText>
+      <tr>
+        {!below680 && <td>{index}</td>}
+        <td>
+          <CustomLink>
+            {/* <FormattedName
+              text={below680 ? item.symbol : item.name}
+              maxCharacters={below600 ? 8 : 16}
+              adjustSize={true}
+              link={true}
+            /> */}
+            {below680 ? item.symbol : item.name}
+          </CustomLink>
+        </td>
         {!below680 && (
-          <DataText area="symbol" color="text" fontWeight="500">
-            <FormattedName text={item.symbol} maxCharacters={5} />
-          </DataText>
+          <td>
+            {/* <FormattedName text={item.symbol} maxCharacters={5} /> */}
+            {item.symbol}
+          </td>
         )}
-        <DataText area="liq">{formattedNum(item.totalLiquidityUSD, true)}</DataText>
-        <DataText area="vol">{formattedNum(item.oneDayVolumeUSD, true)}</DataText>
-        {!below1080 && (
-          <DataText area="price" color="text" fontWeight="500">
-            {formattedNum(item.priceUSD, true)}
-          </DataText>
-        )}
-        {!below1080 && <DataText area="change">{formattedPercent(item.priceChangeUSD)}</DataText>}
-      </DashGrid>
+        <td>{formattedNum(item.totalLiquidityUSD, true)}</td>
+        <td>{formattedNum(item.oneDayVolumeUSD, true)}</td>
+        {!below1080 && <td>{formattedNum(item.priceUSD, true)}</td>}
+        {!below1080 && <td>{formattedPercent(item.priceChangeUSD)}</td>}
+      </tr>
     )
   }
 
   return (
     <ListWrapper>
-      <DashGrid center={true} style={{ height: 'fit-content', padding: '0 1.125rem 1rem 1.125rem' }}>
-        <Flex alignItems="center" justifyContent="flexStart">
-          <ClickableText
-            color="text"
-            area="name"
-            fontWeight="500"
-            onClick={(e) => {
-              setSortedColumn(SORT_FIELD.NAME)
-              setSortDirection(sortedColumn !== SORT_FIELD.NAME ? true : !sortDirection)
-            }}
-          >
-            {below680 ? 'Symbol' : 'Name'} {sortedColumn === SORT_FIELD.NAME ? (!sortDirection ? '↑' : '↓') : ''}
-          </ClickableText>
-        </Flex>
-        {!below680 && (
-          <Flex alignItems="center">
+      <Table>
+        <thead>
+          {!below680 && <th></th>}
+          <th>
             <ClickableText
-              area="symbol"
-              onClick={() => {
-                setSortedColumn(SORT_FIELD.SYMBOL)
-                setSortDirection(sortedColumn !== SORT_FIELD.SYMBOL ? true : !sortDirection)
+              color="text"
+              area="name"
+              fontWeight="500"
+              onClick={(e) => {
+                setSortedColumn(SORT_FIELD.NAME)
+                setSortDirection(sortedColumn !== SORT_FIELD.NAME ? true : !sortDirection)
               }}
             >
-              Symbol {sortedColumn === SORT_FIELD.SYMBOL ? (!sortDirection ? '↑' : '↓') : ''}
+              {below680 ? 'Symbol' : 'Name'} {sortedColumn === SORT_FIELD.NAME ? (!sortDirection ? '↑' : '↓') : ''}
             </ClickableText>
-          </Flex>
-        )}
+          </th>
+          {!below680 && (
+            <th>
+              <ClickableText
+                area="symbol"
+                onClick={() => {
+                  setSortedColumn(SORT_FIELD.SYMBOL)
+                  setSortDirection(sortedColumn !== SORT_FIELD.SYMBOL ? true : !sortDirection)
+                }}
+              >
+                Symbol {sortedColumn === SORT_FIELD.SYMBOL ? (!sortDirection ? '↑' : '↓') : ''}
+              </ClickableText>
+            </th>
+          )}
 
-        <Flex alignItems="center">
-          <ClickableText
-            area="liq"
-            onClick={(e) => {
-              setSortedColumn(SORT_FIELD.LIQ)
-              setSortDirection(sortedColumn !== SORT_FIELD.LIQ ? true : !sortDirection)
-            }}
-          >
-            Liquidity {sortedColumn === SORT_FIELD.LIQ ? (!sortDirection ? '↑' : '↓') : ''}
-          </ClickableText>
-        </Flex>
-        <Flex alignItems="center">
-          <ClickableText
-            area="vol"
-            onClick={() => {
-              setSortedColumn(useTracked ? SORT_FIELD.VOL_UT : SORT_FIELD.VOL)
-              setSortDirection(
-                sortedColumn !== (useTracked ? SORT_FIELD.VOL_UT : SORT_FIELD.VOL) ? true : !sortDirection
-              )
-            }}
-          >
-            Volume (24hrs)
-            {sortedColumn === (useTracked ? SORT_FIELD.VOL_UT : SORT_FIELD.VOL) ? (!sortDirection ? '↑' : '↓') : ''}
-          </ClickableText>
-        </Flex>
-        {!below1080 && (
-          <Flex alignItems="center">
+          <th>
             <ClickableText
-              area="price"
+              area="liq"
               onClick={(e) => {
-                setSortedColumn(SORT_FIELD.PRICE)
-                setSortDirection(sortedColumn !== SORT_FIELD.PRICE ? true : !sortDirection)
+                setSortedColumn(SORT_FIELD.LIQ)
+                setSortDirection(sortedColumn !== SORT_FIELD.LIQ ? true : !sortDirection)
               }}
             >
-              Price {sortedColumn === SORT_FIELD.PRICE ? (!sortDirection ? '↑' : '↓') : ''}
+              Liquidity {sortedColumn === SORT_FIELD.LIQ ? (!sortDirection ? '↑' : '↓') : ''}
             </ClickableText>
-          </Flex>
-        )}
-        {!below1080 && (
-          <Flex alignItems="center">
+          </th>
+          <th>
             <ClickableText
-              area="change"
-              onClick={(e) => {
-                setSortedColumn(SORT_FIELD.CHANGE)
-                setSortDirection(sortedColumn !== SORT_FIELD.CHANGE ? true : !sortDirection)
+              area="vol"
+              onClick={() => {
+                setSortedColumn(useTracked ? SORT_FIELD.VOL_UT : SORT_FIELD.VOL)
+                setSortDirection(
+                  sortedColumn !== (useTracked ? SORT_FIELD.VOL_UT : SORT_FIELD.VOL) ? true : !sortDirection
+                )
               }}
             >
-              Price Change (24hrs)
-              {sortedColumn === SORT_FIELD.CHANGE ? (!sortDirection ? '↑' : '↓') : ''}
+              Volume (24hrs)
+              {sortedColumn === (useTracked ? SORT_FIELD.VOL_UT : SORT_FIELD.VOL) ? (!sortDirection ? '↑' : '↓') : ''}
             </ClickableText>
-          </Flex>
-        )}
-      </DashGrid>
-      <Divider />
-      <List p={0}>
-        {filteredList &&
-          filteredList.map((item, index) => {
-            return (
-              <div key={index}>
-                <ListItem key={index} index={(page - 1) * itemMax + index + 1} item={item} />
-                <Divider />
-              </div>
-            )
-          })}
-      </List>
+          </th>
+          {!below1080 && (
+            <th>
+              <ClickableText
+                area="price"
+                onClick={(e) => {
+                  setSortedColumn(SORT_FIELD.PRICE)
+                  setSortDirection(sortedColumn !== SORT_FIELD.PRICE ? true : !sortDirection)
+                }}
+              >
+                Price {sortedColumn === SORT_FIELD.PRICE ? (!sortDirection ? '↑' : '↓') : ''}
+              </ClickableText>
+            </th>
+          )}
+          {!below1080 && (
+            <th>
+              <ClickableText
+                area="change"
+                onClick={(e) => {
+                  setSortedColumn(SORT_FIELD.CHANGE)
+                  setSortDirection(sortedColumn !== SORT_FIELD.CHANGE ? true : !sortDirection)
+                }}
+              >
+                Price Change (24hrs)
+                {sortedColumn === SORT_FIELD.CHANGE ? (!sortDirection ? '↑' : '↓') : ''}
+              </ClickableText>
+            </th>
+          )}
+        </thead>
+        <tbody>
+          {filteredList &&
+            filteredList.map((item, index) => {
+              return <ListItem key={index} index={(page - 1) * itemMax + index + 1} item={item} />
+            })}
+        </tbody>
+      </Table>
       <PageButtons>
         <div onClick={() => setPage(page === 1 ? page : page - 1)}>
           <Arrow faded={page === 1 ? true : false}>←</Arrow>
